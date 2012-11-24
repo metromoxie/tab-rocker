e58d0b7c4c352b533ddcead87effbfdfe33dc8f6
Joel Weinberger <jww@cs.berkeley.edu>
2012-11-24 17:35:07 -0500

    Started options page

---
+++ options.js
@@ -0,0 +1,30 @@
+function save_options() {
+	var binding = {
+		"ctrl" = true,
+		"meta" = false,
+		"keycode" = 66;
+	}
+	var ctrl = true;
+	var meta = false;
+	var keybinding = 66;
+
+	chrome.storage.local.set({'binding': binding}, function () {
+		if (chrome.runtime.lastError) {
+			// There was an error. For now, log there was an error and return.
+			console.error('There was an error saving the settings for ' +
+			  'the Tab Rocker extension: ' + chrome.runtime.lastError);
+			return;
+		}
+	});
+}
+
+function restore_options() {
+	chrome.storage.local.get('binding', function(binding) {
+		if (chrome.runtime.lastError) {
+			// There was an error. For now, log there was an error and return.
+			console.error('There was an error retrieving the settings for ' +
+			  'the Tab Rocker extension: ' + chrome.runtime.lastError);
+			return;
+		}
+	});
+}
