de815b501011a7c9132145f434c354ba8f4a77f9
Joel Weinberger <jww@cs.berkeley.edu>
2012-11-26 11:47:38 -0500

    Welcome page binding message

--- welcome.js
+++ welcome.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-	$('#reloadtabs').click(function () {
+	$('#reload-tabs').click(function () {
 		// TODO Reload and close all tabs here
+	});
+
+	// Make the options link open in a new tab.
+	$('#options-anchor').click(function() {
+		console.log('foobar');
+		chrome.tabs.create({ 'url': 'options.html' });
 	});
 });
