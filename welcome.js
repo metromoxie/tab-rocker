5bb941ad2ee5ca5c2346c03cfbcbe4e756015381
Joel Weinberger <jww@cs.berkeley.edu>
2012-11-28 16:58:26 -0800

    Added close and reload functionality to the welcome page.

--- welcome.js
+++ welcome.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
 	$('#reload-tabs').click(function () {
-		// TODO Reload and close all tabs here
+		chrome.tabs.query({}, function (alltabs) {
+			var i;
+			for (i in alltabs) {
+				chrome.tabs.reload(alltabs[i].id);
+			}
+
+			chrome.tabs.getCurrent(function (tab) {
+				chrome.tabs.remove([ tab.id ]);
+			});
+		});
 	});
 
 	// Make the options link open in a new tab.
 	$('#options-anchor').click(function() {
-		console.log('foobar');
 		chrome.tabs.create({ 'url': 'options.html' });
 	});
 });
